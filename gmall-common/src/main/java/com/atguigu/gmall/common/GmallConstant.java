0af938472d2abc6bc285ce31b786402ef055eaaa
myapec <2265261642@qq.com>
2019-12-15 14:00:01 +0800

    modify

---
+++ gmall-common/src/main/java/com/atguigu/gmall/common/GmallConstant.java
@@ -0,0 +1,6 @@
+package com.atguigu.gmall.common;
+
+public class GmallConstant {
+    public static final String  KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    public static final String  KAFKA_TOPIC_EVENT="GMALL_EVENT";
+}
